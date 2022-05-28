10f7db78951c8601e7bfd1d355f6f1f32c91af7a
Abonty18 <labibafarah2998@gmail.com>
2022-05-28 13:07:01 +0600

    4th commit

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,10 @@ const express=require("express");
 const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 const app=express();
-
+const pinRoute=require("./routes/pins")
 dotenv.config();
 
+app.use(express.json())
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>{
      console.log("Woah!MongoDB Connected");
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:
 })
 .catch((err)=>console.log(err));
 
+
+app.use("/api/pins",pinRoute);
 app.listen(8800,()=>{
      console.log("Hey Labiba!Your backend server is running hehe :)!")
 });
